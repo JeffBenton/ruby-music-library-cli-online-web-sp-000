d19e5f0bfe486fbd2a69a532f369208736b57ab1
Jeff Benton <sikletz@gmail.com>
2019-03-14 09:16:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist = gets
-    binding.pry
-    Song.all.collect { |song| song.artist.name = artist }.sort { |a, b| a.name <=> b.name }.each_with_index { |song, i| puts "#{i+1}. #{song.name} - #{song.genre.name}" }
+    # binding.pry
+    # Song.all.collect { |song| song.artist.name = artist }.sort { |a, b| a.name <=> b.name }.each_with_index { |song, i| puts "#{i+1}. #{song.name} - #{song.genre.name}" }
   end
 end
