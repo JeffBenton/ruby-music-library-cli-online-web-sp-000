4cdd671771c864a2ec1589b64c4dc8ab6d371b68
Jeff Benton <sikletz@gmail.com>
2019-03-14 10:15:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -49,6 +49,7 @@ class MusicLibraryController
     num = gets
     songs = Song.all.sort { |a, b| a.name <=> b.name }
     if num.is_a? Integer
+      binding.pry
       puts "Playing #{songs[num-1].name} by #{songs[num-1].artist.name}"
     end
   end
