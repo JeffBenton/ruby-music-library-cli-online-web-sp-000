85386095938d9bba6dc24ff0e9b25a9375fa67c6
Jeff Benton <sikletz@gmail.com>
2019-03-14 09:00:59 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    songs = Song.all.
+    binding.pry
     Song.all.each_with_index { |song, i| puts "#{i+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}" }
   end
 end
