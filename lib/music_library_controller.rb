5f740ecd37f4b9dbac74bd4ef4c10e4392c10910
Jeff Benton <sikletz@gmail.com>
2019-03-14 09:37:37 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -3,6 +3,7 @@ require_relative './music_importer.rb'
 require_relative './song.rb'
 require_relative './genre.rb'
 require_relative './artist.rb'
+require_relative './concerns'
 class MusicLibraryController
   def initialize(path = "./db/mp3s")
     MusicImporter.new(path).import
