ca63a4b5b5a954d28f996fe520dd7dca9d266e4d
Jeff Benton <sikletz@gmail.com>
2019-03-14 10:08:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,8 +47,8 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     num = gets
-    
     songs = Song.all.sort { |a, b| a.name <=> b.name }
+    if num.is_a? Integer 
     # puts "Playing #{songs[num-1].name} by #{songs[num-1].artist.name}"
   end
 end
