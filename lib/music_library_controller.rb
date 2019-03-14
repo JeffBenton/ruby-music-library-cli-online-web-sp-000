7e8cb101db90f32814c18bb6e9e6be692e44b9da
Jeff Benton <sikletz@gmail.com>
2019-03-14 09:49:01 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,11 +41,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist = gets.strip!
-    # Song.all.collect { |song| song if song.artist.name == artist }.sort { |a, b| a.name <=> b.name }.each_with_index { |song, i| puts "#{i+1}. #{song.name} - #{song.genre.name}" }
-    songs = Song.all.delete_if { |song| song if song.artist.name != artist }
-    binding.pry
-    songs.sort! { |a, b| a.name <=> b.name }
-    songs.each_with_index { |song, i| puts "#{i+1}. #{song.name} - #{song.genre.name}" }
+    Song.all.delete_if { |song| song if song.artist.name != artist }.sort { |a, b| a.name <=> b.name }.each_with_index { |song, i| puts "#{i+1}. #{song.name} - #{song.genre.name}" }
   end
 end
 
