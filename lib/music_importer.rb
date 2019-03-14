4aad05ecb7e521338cb798534b43069fb48cb84d
Jeff Benton <sikletz@gmail.com>
2019-03-14 08:18:08 +0000

    Automatically backed up by Learn

---
+++ lib/music_importer.rb
@@ -0,0 +1,12 @@
+require 'pry'
+class MusicImporter
+  attr_reader :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    files = Dir.entries(@path).reject! { |file| file.length < 3 }
+  end
+end
